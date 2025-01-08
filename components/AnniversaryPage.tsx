72ecbc5fe53b8be3f0fc27a0e69db61f1815f6e7
Gabriel Belchior Vieira Campanile <campanile@unifesp.br>
2025-01-08 00:12:34 -0300

    refactor: comment out Header and Footer components in AnniversaryPage

--- components/AnniversaryPage.tsx
+++ components/AnniversaryPage.tsx
@@ -20,14 +20,14 @@ export default function AnniversaryPage() {
 
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
-      <Header />
+      {/* <Header /> */}
       <main className="flex-grow container mx-auto px-4 py-8">
         <h1 className="text-4xl font-bold text-center text-white mb-8">Nosso 3º Aniversário</h1>
         <CountdownTimer startDate="2022-01-01" />
         <ImageGallery />
         <BackgroundMusic />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
