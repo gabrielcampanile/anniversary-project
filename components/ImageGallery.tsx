56048e811e5a0ead1eb13fac8dd3847b0358c929
Gabriel Belchior Vieira Campanile <campanile@unifesp.br>
2025-01-07 23:24:40 -0300

    feat: :sparkles: enhance ImageGallery with memory details and sorting

--- components/ImageGallery.tsx
+++ components/ImageGallery.tsx
@@ -1,24 +1,122 @@
+'use client'
+
 import Image from 'next/image'
 
-const images = [
-    '/aviao.jpg',
-    '/praia.jpg',
-    '/paraquedismo.jpg',
-  // Adicione mais URLs de imagens conforme necessário
+interface Memory {
+  id: number
+  imageUrl: string
+  location: string
+  date: string
+  description: string
+}
+
+const memories: Memory[] = [
+  {
+    id: 1,
+    imageUrl: '/aviao.jpg',
+    location: 'Boituva, SP',
+    date: '2021-11-20',
+    description: 'Onde tudo começou'
+  },
+  {
+    id: 2,
+    imageUrl: '/praia.jpg',
+    location: 'Peruíbe, SP',
+    date: '2022-02-28',
+    description: 'Nossa primeira viagem para a praia'
+  },
+  {
+    id: 3,
+    imageUrl: '/paraquedismo.jpg',
+    location: 'Boituva, SP',
+    date: '2022-01-26',
+    description: 'Primeiro pôr do sol'
+  },
+  {
+    id: 4,
+    imageUrl: '/acad.jpg',
+    location: 'Boituva, SP',
+    date: '2023-03-09',
+    description: 'Primeiro treino juntos'
+  },
+  {
+    id: 5,
+    imageUrl: '/campos.jpg',
+    location: 'Campos do Jordão, SP',
+    date: '2024-11-15',
+    description: 'Primeira viagem internacional rs'
+  },
+  {
+    id: 6,
+    imageUrl: '/friends.jpg',
+    location: "Manu's House",
+    date: '2023-12-30',
+    description: 'Primeiro reveillon com nossos amigos'
+  },
+  {
+    id: 7,
+    imageUrl: '/objetivo.jpg',
+    location: 'Boituva, SP',
+    date: '2019-06-15',
+    description: 'Estudiantes'
+  },
+  {
+    id: 8,
+    imageUrl: '/pisci.jpg',
+    location: 'Iperó, SP',
+    date: '2024-01-01',
+    description: 'Aniversário de 2 anos'
+  },
+  {
+    id: 9,
+    imageUrl: '/praia2.jpg',
+    location: 'Itanhaém, SP',
+    date: '2023-11-18',
+    description: 'Primeira viagem sozinhos'
+  },
+  {
+    id: 10,
+    imageUrl: '/show.jpg',
+    location: 'Alfenas, MG',
+    date: '2024-06-22',
+    description: 'Primeiro show 🤘'
+  },
+  {
+    id: 11,
+    imageUrl: '/tranca.jpg',
+    location: "Manu's House",
+    date: '2022-09-17',
+    description: 'Aprendendo a jogar tranca'
+  },
+  {
+    id: 12,
+    imageUrl: '/work.jpg',
+    location: 'Alfenas, MG',
+    date: '2023-06-22',
+    description: 'Trabalhando nossa sorte'
+  }
 ]
+
+// Ordenar as memórias por data
+memories.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
 
 export default function ImageGallery() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
-      {images.map((src, index) => (
-        <div key={index} className="relative h-64 rounded-lg overflow-hidden">
+      {memories.map(memory => (
+        <div key={memory.id} className="relative h-64 rounded-lg overflow-hidden">
           <Image
-            src={src}
-            alt={`Anniversary photo ${index + 1}`}
+            src={memory.imageUrl}
+            alt={memory.description}
             layout="fill"
             objectFit="cover"
             className="transition-transform duration-300 hover:scale-110"
           />
+          <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-2">
+            <h3 className="text-lg font-semibold">{memory.location}</h3>
+            <p className="text-sm">{memory.date}</p>
+            <p className="text-sm">{memory.description}</p>
+          </div>
         </div>
       ))}
     </div>
